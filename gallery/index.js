3b6b46efcffb48d7c1e8a07b6ee9eecfa129c65d
davidbrzy <brzy@studiodenali.cz>
2022-07-14 18:24:10 +0200

    update 4 files and delete 273 files

--- gallery/index.js
+++ gallery/index.js
@@ -12,36 +12,14 @@ const storage = multer.diskStorage({
         console.log(file)
         cb(null, Date.now() + path.extname(file.originalname))
     }
-})
-
+});
 const upload = multer({storage: storage});
 
 app.set("view engine", "ejs");
-app.use("/static", express.static(path.join(__dirname, "uploads")));
 
 app.get("/upload", (req, res) => {
     res.render("upload");
 });
-
-app.get("/get-images", (req, res) => {
-    let images = getImagesFromDir(path.join(__dirname, "uploads"));
-    res.render("index", { title: "Galerie" })
-});
-
-function getImagesFromDir(dirPath) {
-    let allImages = [];
-    let files = fs.readdirSync(dirPath);
-    for (file of files) {
-        let fileLocation = path.join(dirPath, file);
-        var stat = fs.statSync(fileLocation);
-        if (stat && stat.isDirectory()) {
-            getImagesFromDir(fileLocation);
-        } else if (stat && stat.isFile() && [".jpg", ".png"].indexOf(path.extname(fileLocation))) {
-            allImages.push("static/" + file);
-        }
-    }
-    return allImages
-}
 
 app.post("/upload", upload.single("image"), (req, res) => {
     res.send("Image uploaded");
