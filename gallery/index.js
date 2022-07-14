e1a525039ad7cb50bb8cc318b961f44386cb1249
davidbrzy <brzy@studiodenali.cz>
2022-07-14 11:45:56 +0200

    build(deps): update gallery/index.js, package-lock.json and package.json

--- gallery/index.js
+++ gallery/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
 const express = require("express");
@@ -16,14 +17,35 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 app.set("view engine", "ejs");
+app.use("/static", express.static(path.join(__dirname, "uploads")));
 
 app.get("/upload", (req, res) => {
     res.render("upload");
 });
 
+app.get("/get-images", (req, res) => {
+    let images = getImagesFromDir(path.join(__dirname, "uploads"));
+    res.render("index", { title: "Galerie" })
+});
+
+function getImagesFromDir(dirPath) {
+    let allImages = [];
+    let files = fs.readdirSync(dirPath);
+    for (file of files) {
+        let fileLocation = path.join(dirPath, file);
+        var stat = fs.statSync(fileLocation);
+        if (stat && stat.isDirectory()) {
+            getImagesFromDir(fileLocation);
+        } else if (stat && stat.isFile() && [".jpg", ".png"].indexOf(path.extname(fileLocation))) {
+            allImages.push("static/" + file);
+        }
+    }
+    return allImages
+}
+
 app.post("/upload", upload.single("image"), (req, res) => {
     res.send("Image uploaded");
 });
 
-app.listen(3001);
-console.log("3001 is the port");
+app.listen(8000);
+console.log("Listening on port 8000");
