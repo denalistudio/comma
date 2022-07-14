4f1a51ee0d8cfc8e88495e734e6c2b6505e471ca
davidbrzy <brzy@studiodenali.cz>
2022-07-14 18:59:53 +0200

    delete 242 files and update 22 files

---
+++ gallery/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, Application } from "express";
+import multer from "multer";
+const app:Application = express();
+const PORT = process.env.port || 8000;
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads")
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, file.originalname)
+    }
+});
+const upload = multer({ storage: storage });
+
+app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static("uploads"));
+
+app.post("/upload", upload.array("images", 12), (req, res, next) => {
+    var response = '<a href="/">Home</a>';
+    response += "Files uploaded succesfully.";
+    for (var i = 0; i < req.files.length; i++) {
+        response += `<img src="${req.files[i].path}">`;
+    }
+    return res.send(response);
+});
+
+app.listen(PORT, ():void => console.log(`Server is running on port ${PORT}!`))
