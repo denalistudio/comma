9654864a267475206a6b8626c70744442034e862
davidbrzy <brzy@studiodenali.cz>
2022-07-18 13:52:35 +0200

    update 3 files and delete 3 files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import path from "path";
 import express, { Request, Response, Application } from "express";
 import multer from "multer";
@@ -16,9 +17,47 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-app.use(express.static("public"));
+app.use(express.static("views"));
 app.use("/uploads", express.static("uploads"));
-app.use("/uploads", serveIndex(path.join(__dirname, '/uploads')));
+app.use("/uploads", serveIndex(path.join(__dirname, "/uploads")));
+
+app.set("views", path.join("views"));
+app.set("view engine", "ejs");
+
+/*fs.readdir("uploads", (err, images) => {
+    //handling error
+    if (err) {
+        return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    images.forEach((image) => {
+        // Do whatever you want to do with the file
+        console.log(image);
+    });
+});*/
+
+app.get("/", (req, res) => {
+    fs.readdir("uploads", (err, result) => {
+        if (err) {
+            return console.log("Unable to scan directory: " + err);
+        } else if (result && result.length) {
+            const images = [];
+            for (const item of result) {
+                const img = "/upload/" + item;
+                images.push({ img });
+            }
+
+            res.render("index", {
+                title: "Studio Denali CMS",
+                images
+            })
+        } else {
+            res.render("index", {
+                images: []
+            })
+        }
+    })
+})
 
 app.post("/upload", upload.array("images", 12), (req: any, res, next) => {
     var response = '<a href="/">Home</a>';
